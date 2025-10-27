06c9b707f8f9b8dab0b2fdecdd603b4e6a6d244d
Travis Dahlke <travis@dahlke.me>
2025-10-27 13:56:05 -0500

    Update zshrc

--- .setup.sh
+++ .setup.sh
@@ -12,10 +12,6 @@ if which -s brew; then
     echo 'Homebrew is already installed'
 else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    (
-        echo
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-    ) >>$HOME/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
